2eacc1cfae57323631be765561f1d669789cd4b9
crazyyy <crazyyy@gmail.com>
2016-10-24 07:02:12 +0300

    front page okok

--- wordpress/wp-content/themes/wp-antiddos/footer.php
+++ wordpress/wp-content/themes/wp-antiddos/footer.php
@@ -1,19 +1,61 @@
-    </div><!-- /.inner -->
-  </section><!-- /section -->
 
+  </section><!-- /section -->
 </div><!-- /wrapper -->
 
 <footer role="contentinfo">
-  <div class="inner">
+  <div class="container">
+    <div class="row">
 
-    <p class="copyright">
-      &copy; <?php echo date("Y"); ?> Собственность <?php bloginfo('name'); ?>.
-    </p><!-- /copyright -->
+      <div class="col-md-3 footer--first footer--adress">
+        <p>Kiev, Mamontova, 15</p>
+        <p>DDoS Protections Ltd</p>
+      </div><!-- /footer--adress -->
 
-  </div><!-- /.inner -->
+      <div class="col-md-3 footer--first footer--phone">
+        <a href="tel:+380947112310">+38 (094) 711 23 10</a>
+        <a href="tel:+380947112315">+38 (094) 711 23 15</a>
+      </div><!-- /.col-md-3 footer--phone -->
+
+      <div class="col-md-3 footer--first footer--mail">
+        <a href="mailto:info@antiddos.eu">info@antiddos.eu</a>
+        <a href="mailto:question@antiddos.eu">question@antiddos.eu</a>
+      </div><!-- /.col-md-3 footer--mail -->
+
+      <div class="col-md-3 footer--first footer--social">
+        <a href="#">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="#">
+          <i class="fa fa-google-plus" aria-hidden="true"></i>
+        </a>
+      </div><!-- /.col-md-3 footer--social -->
+    </div><!-- /.row -->
+
+    <div class="row">
+      <div class="col-md-3 footer--second">
+        <a href="#">Политика использования</a>
+      </div><!-- /.col-md-3 footer--second -->
+      <div class="col-md-3 footer--second">
+        <a href="#">Соглашение об уровне услуг</a>
+      </div><!-- /.col-md-3 footer--second -->
+
+      <div class="col-md-3 footer--second">
+        <a href="#">Политика обработки  данных</a>
+      </div><!-- /.col-md-3 footer--second -->
+    </div><!-- /.row -->
+
+    <div class="row">
+      <div class="col-md-12 footer--last">
+        <p>&copy; <?php echo date("Y"); ?> All rights reserved</p>
+      </div><!-- /.col-md-12 footer-last -->
+    </div><!-- /.row -->
+  </div><!-- /.container -->
 </footer><!-- /footer -->
 
-    <?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 
 </body>
 </html>
--- wordpress/wp-content/themes/wp-antiddos/header.php
+++ wordpress/wp-content/themes/wp-antiddos/header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
+  <!-- saved from url=(0014)about:internet -->
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,30 +20,48 @@
   <![endif]-->
   <!-- css + javascript -->
   <?php wp_head(); ?>
+
 </head>
 <body <?php body_class(); ?>>
 <!-- wrapper -->
 <div class="wrapper">
   <header role="banner">
-    <div class="inner">
+    <div class="container">
+      <div class="row">
 
-      <div class="logo">
-        <?php if ( is_front_page() && is_home() ){ } else { ?>
-          <a href="<?php echo home_url(); ?>">
-            <?php  } ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-            <?php if ( is_front_page() && is_home() ){
-            } else { ?>
-          </a>
-        <?php } ?>
-      </div><!-- /logo -->
+        <div class="col-md-3 col-md-offset-3 header-top--phone">
+          звоните: <a href="tel:+380947112310">+38 (094) 711 23 10</a>
+        </div><!-- /.col-md-3 col-md-offset-3 header-top--phone -->
 
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
+        <div class="col-md-3 header-top--mail">
+          пишите: <a href="mailto:info@antiddos.eu">info@antiddos.eu</a>
+        </div><!-- /.col-md-3 header-top--mail -->
 
-    </div><!-- /.inner -->
+        <div class="col-md-3 header-top--security">
+          <a href="#" class="stand-link"><span>Встать под защиту</span></a>
+          <ul class="change-lang">
+            <li>EN</li>
+            <li>RU</li>
+          </ul><!-- change-lang -->
+        </div><!-- /.col-md-3 header-top--security -->
+
+        <div class="logo col-md-3">
+          <?php if ( is_front_page() && is_home() ){ } else { ?>
+            <a href="<?php echo home_url(); ?>">
+              <?php  } ?>
+              <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
+              <?php if ( is_front_page() && is_home() ){
+              } else { ?>
+            </a>
+          <?php } ?>
+        </div><!-- /logo -->
+
+        <nav class="header--nav col-md-9" role="navigation">
+          <?php wpeHeadNav(); ?>
+        </nav><!-- /header--nav -->
+
+      </div><!-- /.row -->
+    </div><!-- /.container -->
   </header><!-- /header -->
 
   <section role="main">
-    <div class="inner">
