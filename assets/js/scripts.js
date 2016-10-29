be30b493392e5f9b0e0dc6fdad957597b33a71f8
crazyyy <crazyyy@gmail.com>
2016-10-30 01:25:17 +0300

    new construct

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -165,11 +165,64 @@ $(document).ready(function() {
       // vps
       else if ( ( typeVisit == 1 ) && ( typeServ == 2 ) || ( typeVisit == 2 ) && ( typeServ == 2 ) ) {
         choosedPackage = 'VPS1';
+      } else if ( ( typeVisit == 3 ) && ( typeServ == 2 ) || ( typeVisit == 3 ) && ( typeServ == 2 ) ) {
+        choosedPackage = 'VPS2';
+      } else if ( ( typeVisit == 5 ) && ( typeServ == 2 ) || ( typeVisit == 6 ) && ( typeServ == 2 ) ) {
+        choosedPackage = 'VPS3';
       }
       // dedic
       else if ( ( typeVisit == 1 ) && ( typeServ == 1 ) || ( typeVisit == 2 ) && ( typeServ == 1 ) ) {
         choosedPackage = 'Dedicated1';
+      } else if ( ( typeVisit == 3 ) && ( typeServ == 1 ) || ( typeVisit == 4 ) && ( typeServ == 1 ) ) {
+        choosedPackage = 'Dedicated2';
+      } else if ( ( typeVisit == 5 ) && ( typeServ == 1 ) || ( typeVisit == 6 ) && ( typeServ == 1 ) ) {
+        choosedPackage = 'Dedicated2';
       }
+      // remote
+      else if ( ( typeVisit == 1 ) && ( typeServ == 4 ) || ( typeVisit == 2 ) && ( typeServ == 4 ) ) {
+        choosedPackage = 'Tunnel1';
+      } else if ( ( typeVisit == 3 ) && ( typeServ == 4 ) || ( typeVisit == 4 ) && ( typeServ == 4 ) ) {
+        choosedPackage = 'Tunnel2';
+      } else if ( ( typeVisit == 5 ) && ( typeServ == 4 ) || ( typeVisit == 6 ) && ( typeServ == 4 ) ) {
+        choosedPackage = 'Tunnel3';
+      }
+      // cluster
+      else if ( ( typeVisit == 1 ) && ( typeServ == 5 ) || ( typeVisit == 2 ) && ( typeServ == 5 ) ) {
+        choosedPackage = 'Dedicated1';
+      } else if ( ( typeVisit == 3 ) && ( typeServ == 5 ) || ( typeVisit == 4 ) && ( typeServ == 5 ) ) {
+        choosedPackage = 'Dedicated2';
+      } else if ( ( typeVisit == 5 ) && ( typeServ == 5 ) || ( typeVisit == 6 ) && ( typeServ == 5 ) ) {
+        choosedPackage = 'Dedicated3';
+      }
+      // unknown
+      else if ( ( typeVisit == 1 ) && ( typeServ == 6 ) || ( typeVisit == 2 ) && ( typeServ == 6 ) ) {
+        choosedPackage = 'Hosting1';
+      } else if ( ( typeVisit == 3 ) && ( typeServ == 6 ) || ( typeVisit == 4 ) && ( typeServ == 6 ) ) {
+        choosedPackage = 'Hosting2';
+      } else if ( ( typeVisit == 5 ) && ( typeServ == 6 ) || ( typeVisit == 6 ) && ( typeServ == 6 ) ) {
+        choosedPackage = 'Hosting3';
+      }
+    } else if ( typeType == 2 ) {
+      // // shared
+      // if ( ( typeVisit == 1 ) && ( typeServ == 3 ) || ( typeVisit == 2 ) && ( typeServ == 3 ) ) {
+      //   choosedPackage = 'Hosting1';
+      // } else if ( ( typeVisit == 3 ) && ( typeServ == 3 ) || ( typeVisit == 4 ) && ( typeServ == 3 ) ) {
+      //   choosedPackage = 'Hosting2';
+      // } else if ( ( typeVisit == 5 ) && ( typeServ == 3 ) || ( typeVisit == 6 ) && ( typeServ == 3 ) ) {
+      //   choosedPackage = 'Hosting3';
+      // }
+      // // vps
+      // else if ( ( typeVisit == 1 ) && ( typeServ == 2 ) || ( typeVisit == 2 ) && ( typeServ == 2 ) ) {
+      //   choosedPackage = 'VPS1';
+      // } else if ( ( typeVisit == 3 ) && ( typeServ == 2 ) || ( typeVisit == 4 ) && ( typeServ == 2 ) ) {
+      //   choosedPackage = 'VPS2';
+      // } else if ( ( typeVisit == 5 ) && ( typeServ == 2 ) || ( typeVisit == 6 ) && ( typeServ == 2 ) ) {
+      //   choosedPackage = 'VPS3';
+      // }
+      // // dedic
+      // else if ( ( typeVisit == 1 ) && ( typeServ == 1 ) || ( typeVisit == 2 ) && ( typeServ == 1 ) ) {
+      //   choosedPackage = 'Dedicated1';
+      // }
     }
 
 
